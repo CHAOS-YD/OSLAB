67e9b7d0d2e2a1b0831717613825b77b27911250
qdyangdi <936768863@qq.com>
2023-05-29 11:22:40 +0800

    Enable Paging mode.

--- src/head.s
+++ src/head.s
@@ -1,5 +1,6 @@
 %include "boot.inc"
 %include "page.inc"
+org (SYSSEG << 4)
 USE32
 _start:
     pusha
@@ -8,7 +9,7 @@ _start:
     mov bl,0x0e
     mov dh,0x3
     mov dl,0x0
-    mov esi,msg_head + (SYSSEG << 4)
+    mov esi,msg_head
     
     call print_cstr32
     popa
@@ -24,13 +25,41 @@ _setup_page:
     mov ebx,page_dir + (PDE_SELF_REFERENCE * ENTRY_SIZE)
     mov [ebx],eax
 .set_page_32mb:
-    xchg bx,bx
+    
     mov ecx,ENTRY_COUNTS * 8
     xor eax,eax
     mov edi,page_tbl
     cld
-    rep stosw
+    rep stosd
+    
+    ;xchg bx,bx
+    
+    mov dword [page_dir + (0 * ENTRY_SIZE)],page_tbl + (0 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (1 * ENTRY_SIZE)],page_tbl + (1 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (2 * ENTRY_SIZE)],page_tbl + (2 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (3 * ENTRY_SIZE)],page_tbl + (3 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (4 * ENTRY_SIZE)],page_tbl + (4 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (5 * ENTRY_SIZE)],page_tbl + (5 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (6 * ENTRY_SIZE)],page_tbl + (6 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    mov dword [page_dir + (7 * ENTRY_SIZE)],page_tbl + (7 * PAGE_SIZE) + KERNEL_PAGE_FLAGS
+    
+    mov edi,page_tbl + (7 * PAGE_SIZE) + 4092
+    mov eax,0x1fff000 + USER_PAGE_FLAGS   ;32MB - 4KB
+    std
+l:  stosd
+    sub eax,PAGE_SIZE
+    jge l
+    
+.set_pdir:
+    mov eax,page_dir
+    and eax,0xfffff000
+    mov cr3,eax
+.enable_paging:    
+    mov eax,cr0
+    or eax,0x80000000
+    mov cr0,eax
 
+    
     jmp $
 
 
